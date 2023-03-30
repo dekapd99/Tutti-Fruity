e7aeb09d71fc9dbf4090cd41949491f6e3701c31
dekapd99 <dekaprimatio@gmail.com>
2023-03-30 22:03:33 +0700

    Initial Commit

---
+++ Tutti-Fruity/Tutti_FruityApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tutti_FruityApp.swift
+//  Tutti-Fruity
+//
+//  Created by Deka Primatio on 30/03/23.
+//
+
+import SwiftUI
+
+@main
+struct Tutti_FruityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
