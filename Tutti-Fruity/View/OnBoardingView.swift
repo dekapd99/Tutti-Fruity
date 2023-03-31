3ce1553816b9b6c74725c00ca6a847b2669b938a
dekapd99 <dekaprimatio@gmail.com>
2023-03-31 10:53:27 +0700

    Create Array of FruitData, FruitModel for Fruit Data Type Declaration, Integrate FruitData to FruitCardComponent, Fetching FruitData from FruitCardComponent to OnBoardingView within ForEach Loop

--- Tutti-Fruity/View/OnBoardingView.swift
+++ Tutti-Fruity/View/OnBoardingView.swift
@@ -10,14 +10,19 @@ import SwiftUI
 struct OnBoardingView: View {
     //MARK: - PROPERTIES
     
+    var fruits: [FruitModel] = fruitsData //Fetching Data from Local FruitData file with shape of FruitModel Array
+    
     //MARK: - BODY CONTENT
     var body: some View {
-        //MARK: - TABVIEW
+        //MARK: - TABVIEW FOR INITIALIZE FRUIT DATA
         TabView {
-            ForEach(0..<5) { item in
-                FruitCardComponent()
+            
+            //Rendering 6 First Data from Local FruitData files with shape of FruitModel Array
+            ForEach(fruits[0...5]) { item in
+                FruitCardComponent(fruit: item)
             }//: - FOREACH LOOP FOR 5 ITEMS ONLY
-        }//: - TABVIEW
+            
+        }//: - TABVIEW FOR INITIALIZE FRUIT DATA
         .tabViewStyle(PageTabViewStyle())
     }//: - BODY CONTENT
 }
@@ -25,6 +30,7 @@ struct OnBoardingView: View {
 //MARK: - PREVIEW
 struct OnBoardingView_Previews: PreviewProvider {
     static var previews: some View {
-        OnBoardingView()
+        //Passing fruitsData from Local FruitData file
+        OnBoardingView(fruits: fruitsData)
     }
 }
